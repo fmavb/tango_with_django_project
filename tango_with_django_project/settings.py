a58da21f3ede802df76a9aef7b77b11c81b0946e
Alexander Behrmann <2250046b@student.gla.ac.uk>
2018-02-04 13:53:55 +0000

    Chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
+LOGIN_URL = '/rango/login/'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
